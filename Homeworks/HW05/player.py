0b4a2481895c73a9efe5c330a2e9eaa00bd8026d
Jeffrey Ng <jeffreyng99@gmail.com>
2021-04-22 18:02:42 -0500

    added a player name and id for palyers

--- Homeworks/HW05/player.py
+++ Homeworks/HW05/player.py
@@ -1,18 +1,27 @@
-
 class Player:
-    def __init__(self):
+    def __init__(self, name="Player", player_id=0):
         self.__hand = []
+        self.__id = player_id
+        self.__name = name
 
     @property
     def hand(self):
         return self.__hand
 
+    @property
+    def player_id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        return self.__name
+
     def add_card(self, card):
         self.hand.append(card)
 
     def __str__(self):
-        hand = "Hand: ["
+        hand = f"{self.name}{self.player_id}\nHand: ["
         for card in self.hand:
             hand += str(card) + ", "
 
-        return hand.strip(", ") + "]"
+        return hand.rstrip(", ") + "]"
