d16ebccfb25af35d528993bda6844f4e9e63ad0c
Jeffrey Ng <jeffreyng99@gmail.com>
2021-04-22 18:02:04 -0500

    deck can now be adjusted based on how many 52 decks are being used

--- Homeworks/HW05/deck.py
+++ Homeworks/HW05/deck.py
@@ -6,15 +6,16 @@ def shuffle(stuff):
         random.shuffle(stuff)
 
 class Deck:
-    def __init__(self):
+    def __init__(self, num_decks=1):
         self.__deck = []
         faces = range(2, 15)
         suits = [Card.Suit.Diamonds, Card.Suit.Clubs, Card.Suit.Hearts, Card.Suit.Spades]
-        for suit in suits:
-            for face in faces:
-                self.__deck.append(Card(face=face, suit=suit))
+        for _ in range(num_decks):
+            for suit in suits:
+                for face in faces:
+                    self.__deck.append(Card(face=face, suit=suit))
 
-        assert len(self.__deck) == 52
+        assert len(self.__deck) == 52 * num_decks
 
         shuffle(self.__deck)
 
